b758396774797d675c6167f0a43a7d0f236c36d0
Quinn Davis <qdavis6106@stu.d214.org>
2023-12-14 10:57:32 -0600

    Initial Commit

---
+++ Laugh Cry Think App/Laugh_Cry_Think_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Laugh_Cry_Think_AppApp.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Laugh_Cry_Think_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
