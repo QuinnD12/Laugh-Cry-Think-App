3b1c95f08eb20b2db2a19c91a3e02a7cd312980a
Quinn Davis <qdavis6106@stu.d214.org>
2023-12-18 11:20:59 -0600

    Youtube stuff is in

---
+++ Laugh Cry Think App/Temp YouTube View.swift
@@ -0,0 +1,18 @@
+//
+//  Temp YouTube View.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 12/18/23.
+//
+
+import SwiftUI
+
+struct Temp_YouTube_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Temp_YouTube_View()
+}
