ddcc6277d26fba970428fc94a98fa37669684cd7
Quinn Davis <qdavis6106@stu.d214.org>
2023-12-18 11:19:30 -0600

    Youtube stuff is pretyy mutch done

---
+++ Laugh Cry Think App/YouTube Test View(Temporary).swift
@@ -0,0 +1,18 @@
+//
+//  YouTube Test View(Temporary).swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 12/18/23.
+//
+
+import SwiftUI
+
+struct YouTube_Test_View_Temporary_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTube_Test_View_Temporary_()
+}
