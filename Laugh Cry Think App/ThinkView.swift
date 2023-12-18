fb566f66e2cdf401750c7c6ed80a791703898a4d
Efe Ozalp <efeozalp0101@gmail.com>
2023-12-18 10:12:29 -0600

    new files

---
+++ Laugh Cry Think App/ThinkView.swift
@@ -0,0 +1,20 @@
+//
+//  ThinkView.swift
+//  Laugh Cry Think App
+//
+//  Created by Efe Ozalp on 12/18/23.
+//
+
+import SwiftUI
+
+struct ThinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThinkView()
+    }
+}
