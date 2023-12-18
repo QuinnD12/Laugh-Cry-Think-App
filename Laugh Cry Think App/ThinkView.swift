639e99c8149e33a54c6247257aa78a6657da897d
Efe Ozalp <efeozalp0101@gmail.com>
2023-12-18 10:31:54 -0600

    Quotes

--- Laugh Cry Think App/ThinkView.swift
+++ Laugh Cry Think App/ThinkView.swift
@@ -6,10 +6,59 @@
 //
 
 import SwiftUI
+import Combine
 
+struct Quote: Codable {
+    let content: String
+    let author: String
+}
+
+class QuoteViewModel: ObservableObject {
+    @Published var quote: Quote?
+    private var cancellables: Set<AnyCancellable> = []
+    func fetchRandomQuote() {
+        guard let url = URL(string: "https://api.quotable.io/random") else { return }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: Quote.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }, receiveValue: { [weak self] quote in
+                self?.quote = quote
+            })
+            .store(in: &cancellables)
+    }
+}
 struct ThinkView: View {
+    @ObservedObject private var quoteViewModel = QuoteViewModel()
+    @State private var imageIndex = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("landscape\(imageIndex)")
+                .resizable()
+                .scaledToFill()
+                .opacity(0.3)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                if let quote = quoteViewModel.quote {
+                    Text("\(quote.content)")
+                        .font(.title3)
+                        .bold()
+                        .lineLimit(nil)
+                        .frame(maxWidth: 200, maxHeight: 150, alignment: .center)
+                        .minimumScaleFactor(0.5)
+                        .padding()
+                    Text("~ \(quote.author)")
+                        .padding()
+                } else {
+                    Text("Fetching quote...")
+                }
+                Button("Get Another Quote") {
+                    quoteViewModel.fetchRandomQuote()
+                    imageIndex = (imageIndex % 5) + 1
+                }
+                .padding()
+            }
+        }
     }
 }
 
