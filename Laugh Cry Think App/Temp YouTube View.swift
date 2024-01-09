bbec6cc80a1b4b5bd920475dd94b94fef4447f9b
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-09 11:15:25 -0600

    YOutube all done

--- Laugh Cry Think App/Temp YouTube View.swift
+++ Laugh Cry Think App/Temp YouTube View.swift
@@ -17,8 +17,7 @@ struct YouTubeTestView: View {
             Task {
                 do {
                     videoID = try await getVideoID()
-                    done = true
-                    print(videoID ?? "none")
+                    done.toggle()
                 } catch NetworkError.invalidURL {
                     print("Invalid URL")
                 } catch NetworkError.invalidResponse {
@@ -49,7 +48,13 @@ enum NetworkError: Error {
 }
 
 func getVideoID(APIKey: String = "AIzaSyDEu0gqC_zfELf-iGhsmfiH1IZ_KD73Vng") async throws -> String {
-    let endpoint = "https://www.googleapis.com/youtube/v3/search?key=\(APIKey)&channelId=UC_zEzzq54Rm0iy7lmmZbCIg&maxResults=1&safeSearch=strict&type=video&videoDuration=short"
+    
+    let date = randRangeDate(startDate: "2022-05-01T12:00:00-05:00", endDate: "2024-01-08T12:00:00-05:00")
+    
+    print(date)
+    
+    let endpoint = "https://www.googleapis.com/youtube/v3/search?key=\(APIKey)&channelId=UC_zEzzq54Rm0iy7lmmZbCIg&maxResults=1&safeSearch=strict&type=video&videoDuration=short&publishedBefore=\(lastWeek(date))&publishedAfter=\(date)"
+    
     guard let url = URL(string: endpoint) else {
         throw NetworkError.invalidURL
     }
@@ -94,4 +99,36 @@ struct Id: Codable {
     let kind: String
     let videoId: String
 }
+
+func randRangeDate(startDate: String, endDate: String) -> String {
+    let RFC3339DateFormatter = DateFormatter()
+    RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    RFC3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    
+    let startDateRFC3339 = RFC3339DateFormatter.date(from: startDate)
+    let endDateRFC3339 = RFC3339DateFormatter.date(from: endDate)
+    
+    let date = Date(
+        timeIntervalSinceNow: .random(
+            in: startDateRFC3339!.timeIntervalSinceNow...endDateRFC3339!.timeIntervalSinceNow
+        )
+    )
+    
+    return RFC3339DateFormatter.string(from: date)
+}
+
+func lastWeek(_ date: String) -> String {
+    let RFC3339DateFormatter = DateFormatter()
+    RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    RFC3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    
+    var dateRFC3339 = RFC3339DateFormatter.date(from: date)
+    dateRFC3339 = dateRFC3339?.addingTimeInterval(604800)
+    
+    return RFC3339DateFormatter.string(from: dateRFC3339!)
+}
+
+
 
