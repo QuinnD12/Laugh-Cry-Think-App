843e8d1c8d9c014ee86e3d1d36ad8723737f1406
Efe Ozalp <efeozalp0101@gmail.com>
2024-01-18 10:52:14 -0600

    yes

--- Laugh Cry Think App/ContentView.swift
+++ Laugh Cry Think App/ContentView.swift
@@ -23,55 +23,92 @@ struct Triangle: Shape {
     }
 }
 
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.pastelPurple
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    HStack {
+                        Triangle()
+                            .fill(Color.softBlue)
+                            .frame(width: 100, height: 100)
+                            .shadow(radius: 5)
+                            .rotationEffect(.degrees(-90))
+                        Circle()
+                            .frame(width: 150, height: 150)
+                        Triangle()
+                            .fill(Color.softBlue)
+                            .frame(width: 100, height: 100)
+                            .shadow(radius: 5)
+                            .rotationEffect(.degrees(90))
+                    }
+                    Text("January 11, 2024")
+                        .font(.title2)
+                        .bold()
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Laugh")
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 75, alignment: .center)
+                            .padding()
+                            .background(Color.softBlue)
+                            .cornerRadius(30)
+                    }
+                    .padding()
+
+                    NavigationLink(destination: ContentView()) {
+                        Text("Cry")
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 75, alignment: .center)
+                            .padding()
+                            .background(Color.softBlue)
+                            .cornerRadius(30)
+                    }
+                    .padding()
+
+                    NavigationLink(destination: ContentView()) {
+                        Text("Think")
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 75, alignment: .center)
+                            .padding()
+                            .background(Color.softBlue)
+                            .cornerRadius(30)
+                    }
+                    .padding()
+                }
+                .padding()
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.pastelPurple
-                .edgesIgnoringSafeArea(.all)
-
-            VStack {
-                HStack {
-                    Triangle()
-                        .fill(Color.blue)
-                        .frame(width: 100, height: 100)
-                        .shadow(radius: 5)
-                        .rotationEffect(.degrees(-90))
-                    Circle()
-                        .frame(width: 150, height: 150)
-                    Triangle()
-                        .fill(Color.blue)
-                        .frame(width: 100, height: 100)
-                        .shadow(radius: 5)
-                        .rotationEffect(.degrees(90))
+        TabView {
+            MainView()
+                .tabItem {
+                    Label("Streak", systemImage: "flame.fill")
                 }
-                Text("January 11, 2024")
-                    .font(.title2)
-                    .bold()
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .cornerRadius(30)
-                    .padding()
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .cornerRadius(30)
-                    .padding()
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .cornerRadius(30)
-                    .padding()
 
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
-            .padding()
+            Text("Calendar View")
+                .tabItem {
+                    Label("Calendar", systemImage: "calendar")
+                }
+
+            Text("About View")
+                .tabItem {
+                    Label("About", systemImage: "info.circle")
+                }
         }
     }
 }
 
 extension Color {
     static let pastelPurple = Color(red: 206/255, green: 156/255, blue: 236/255)
+    static let softBlue = Color(red: 100/255, green: 149/255, blue: 237/255)
 }
 
 struct ContentView_Previews: PreviewProvider {
