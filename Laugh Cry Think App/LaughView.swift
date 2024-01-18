3e6335fb1634e6509c050799986b282f500b775c
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-18 11:14:13 -0600

    youtube placeholder view

---
+++ Laugh Cry Think App/LaughView.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftUIView.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 1/18/24.
+//
+
+import SwiftUI
+
+struct YouTubeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
