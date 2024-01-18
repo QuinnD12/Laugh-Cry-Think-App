1acb05e0c548050e7dcd96d80b20d52243f56ebd
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-18 11:17:33 -0600

    Laugh View second try

--- Laugh Cry Think App/YouTubeAPI.swift
+++ Laugh Cry Think App/YouTubeAPI.swift
@@ -1,5 +1,5 @@
 //
-//  LaughView.swift
+//  YouTubeAPI.swift
 //  Laugh Cry Think App
 //
 //  Created by Quinn B. Davis on 1/18/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct LaughView: View {
+struct YouTubeAPI: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LaughView()
+    YouTubeAPI()
 }
