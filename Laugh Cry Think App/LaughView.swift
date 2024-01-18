c74acc2f273ec4a1dc9de144f3ad2aebfecebfac
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-18 11:18:05 -0600

    Laugh placeholder

--- Laugh Cry Think App/LaughView.swift
+++ Laugh Cry Think App/LaughView.swift
@@ -1,18 +1,34 @@
 //
-//  LaughView.swift
+//  SwiftUIView.swift
 //  Laugh Cry Think App
 //
 //  Created by Quinn B. Davis on 1/18/24.
 //
 
 import SwiftUI
+import YouTubePlayerKit
 
 struct LaughView: View {
+    //@AppStorage("YTLinks") var YTLinks: [String : String] = ["2022-05-01T12:00:00-05:00": "NpEaa2P7qZI"]
+    @State var laughText = ""
+    var placeHolderID: String? = "NpEaa2P7qZI"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Laugh")
+            
+            YouTubePlayerView(
+                YouTubePlayer(source: .video(id: placeHolderID ?? ""), configuration: .init(
+                    loopEnabled: true
+                ))
+            )
+            
+            TextField("Thoughts Placeholder", text: $laughText)
+        }
     }
 }
 
 #Preview {
     LaughView()
 }
+
