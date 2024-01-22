f89ebb43838c5f0b136a1b91c26016344b244c9c
Efe Ozalp <efeozalp0101@gmail.com>
2024-01-22 10:49:47 -0600

    changes for content view

--- Laugh Cry Think App/ContentView.swift
+++ Laugh Cry Think App/ContentView.swift
@@ -9,16 +9,10 @@ import SwiftUI
 struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
-        // Start from the bottom left
         path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        // Add line to the top middle
         path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        // Add line to the bottom right
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        // Close the path to create the third side of the triangle
         path.closeSubpath()
-
         return path
     }
 }
@@ -33,48 +27,61 @@ struct MainView: View {
                 VStack {
                     HStack {
                         Triangle()
-                            .fill(Color.softBlue)
+                            .fill(Color.softPink)
                             .frame(width: 100, height: 100)
                             .shadow(radius: 5)
                             .rotationEffect(.degrees(-90))
-                        Circle()
-                            .frame(width: 150, height: 150)
+                        Image("AppLogo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 150)
+                                    .clipShape(Circle())
+                                    .padding()
                         Triangle()
-                            .fill(Color.softBlue)
+                            .fill(Color.softPink)
                             .frame(width: 100, height: 100)
                             .shadow(radius: 5)
                             .rotationEffect(.degrees(90))
                     }
                     Text("January 11, 2024")
                         .font(.title2)
+                        .foregroundColor(.white)
                         .bold()
                     
                     NavigationLink(destination: ContentView()) {
-                        Text("Laugh")
-                            .foregroundColor(.white)
+                        HStack{
+                            Text("LAUGH")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.softBlue)
+                                .frame(width: 300, height: 75, alignment: .center)
+                                .padding()
+                                .background(Color.softPink)
+                                .cornerRadius(30)
+                        }
+                    }
+                    .padding()
+
+                    NavigationLink(destination: ContentView()) {
+                        Text("CRY")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.softBlue)
                             .frame(width: 300, height: 75, alignment: .center)
                             .padding()
-                            .background(Color.softBlue)
+                            .background(Color.softPink)
                             .cornerRadius(30)
                     }
                     .padding()
 
                     NavigationLink(destination: ContentView()) {
-                        Text("Cry")
-                            .foregroundColor(.white)
+                        Text("THINK")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.softBlue)
                             .frame(width: 300, height: 75, alignment: .center)
                             .padding()
-                            .background(Color.softBlue)
-                            .cornerRadius(30)
-                    }
-                    .padding()
-
-                    NavigationLink(destination: ContentView()) {
-                        Text("Think")
-                            .foregroundColor(.white)
-                            .frame(width: 300, height: 75, alignment: .center)
-                            .padding()
-                            .background(Color.softBlue)
+                            .background(Color.softPink)
                             .cornerRadius(30)
                     }
                     .padding()
@@ -109,6 +116,7 @@ struct ContentView: View {
 extension Color {
     static let pastelPurple = Color(red: 206/255, green: 156/255, blue: 236/255)
     static let softBlue = Color(red: 100/255, green: 149/255, blue: 237/255)
+    static let softPink = Color(red: 226 / 255, green: 197 / 255, blue: 222 / 255)
 }
 
 struct ContentView_Previews: PreviewProvider {
