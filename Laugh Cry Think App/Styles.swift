83649c54619c0496e1a202ea0bea88954d650812
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-24 10:58:24 -0600

    Start font

---
+++ Laugh Cry Think App/Styles.swift
@@ -0,0 +1,18 @@
+//
+//  Sty;es.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 1/22/24.
+//
+
+import SwiftUI
+
+struct Styles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sty_es()
+}
