42ab9fbbe2553ee0815d6e02db4f6c75ddb8c198
Quinn Davis <qdavis6106@stu.d214.org>
2024-01-30 10:40:54 -0600

    Cry View is back

---
+++ Laugh Cry Think App/CryViewTwo.swift
@@ -0,0 +1,18 @@
+//
+//  CryViewTwo.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 1/30/24.
+//
+
+import SwiftUI
+
+struct CryViewTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CryViewTwo()
+}
