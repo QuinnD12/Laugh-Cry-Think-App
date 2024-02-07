d85ead814b5ce54b119270adaa9701b776bb86e7
Quinn Davis <qdavis6106@stu.d214.org>
2024-02-07 11:09:29 -0600

    BG done?

--- Laugh Cry Think App/Styles.swift
+++ Laugh Cry Think App/Styles.swift
@@ -37,63 +37,42 @@ extension View {
     }
 }
 
-struct Pattern: View {
-    var start: CGPoint = CGPoint(x: 200, y: 200)
-    var scalex: Double = 1
-    var scaley: Double = 1
-    var detail: Double = 6
-    var col: Color = .black
-    var opac: Double = 0.3
-    let rand = Double.random(in: 0...1)
-    
-    func f(_ t: Double) -> CGPoint {
-        let x = cos(t)
-        let y = sin(rand*t)
-        
-        return CGPoint(x: scalex*x, y: (scaley*y))
-    }
-    
-    var body: some View {
-        Path { path in
-            path.move(to: start)
-            for i in 0...Int(pow(10.0, detail)) {
-                let c = Double(i) / pow(10, detail/2)
-                
-                let dv = f(c)
-                
-                let point = CGPoint(x: start.x+dv.x, y: start.y+dv.y)
-                
-                path.addLine(to: point)
-            }
-        }
-            .fill(col)
-            .opacity(opac)
-    }
-}
 
 struct Background: View {
-    var start: CGPoint = CGPoint(x: 500, y: 500)
     var scalex: Double = 500
     var scaley: Double = 500
     var detail: Double = 6
-    var col: Color = .black
+    var col1: [Double] = [173, 66, 245]
+    var col2: [Double] = [136, 72, 240]
+    var strokeCol: [Double] = [136/10, 72/10, 240/10]
     var opac: Double = 0.3
-    var yoff: Double = -15
+    var yoff: Double = 200
     var xoff: Double = 0
-    let rand = Double.random(in: 0.1...1)//Fix range?
+    var anim: Bool = true
+    let rand1 = Double.random(in: 0.1...1)
+    let rand2 = Double.random(in: 0.1...1)
+    let rand3 = Int.random(in: 0...1) == 0 ? -1.0 : 1.0
+    
+    @State var colAnim1 = -25.0
+    @State var colAnim2 = -25.0
     
     func f(_ t: Double) -> CGPoint {
         let x = cos(t)
-        let y = sin(rand*t)
+        let y = rand3 * ((round(rand2-0.2)/cos(t)) + sin(rand1*t))
         
         return CGPoint(x: (scalex*x)+xoff, y: (scaley*y)+yoff)
     }
     
     var body: some View {
         ZStack {
-            Color(red: 146/255, green: 52/255, blue: 235/255)
+            LinearGradient(gradient: Gradient(colors: [
+                Color(red: (col1[0]+2*colAnim1) / 255, green: (col1[1]+colAnim1/2) / 255, blue: (col1[2]+colAnim1) / 255),
+                Color(red: (col2[0]+2*colAnim2) / 255, green: (col2[1]+colAnim2/2) / 255, blue: (col2[2]+colAnim2) / 255)
+            ]), startPoint: .top, endPoint: .bottom)
             
             Path { path in
+                let start = CGPoint(x: f(0).x, y: f(0).y)
+                
                 path.move(to: start)
                 for i in 0...Int(pow(10.0, detail)) {
                     let c = Double(i) / pow(10, detail/2)
@@ -104,8 +83,19 @@ struct Background: View {
                     
                     path.addLine(to: point)
                 }
-            }.fill(col)
+            }.fill(Color(red: strokeCol[0] / 255, green: strokeCol[1] / 255, blue: strokeCol[2] / 255))
             .opacity(opac)
+            .onAppear {
+                print("1:\(rand1), 2:\(rand2), 3:\(rand3)")
+                
+                if anim {
+                    withAnimation(.easeInOut(duration: 6.5).repeatForever(autoreverses: true)) {
+                        colAnim1 = 20
+                        colAnim2 = 30
+                    }
+                }
+            }
+            
         }.ignoresSafeArea()
     }
 }
