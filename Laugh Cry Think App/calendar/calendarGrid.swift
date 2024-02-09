f4f4db62908c813c4458d94d5653c9857a0e276c
letiiita <lrios4400@stu.d214.org>
2024-02-09 10:17:56 -0600

    calendar

---
+++ Laugh Cry Think App/calendar/calendarGrid.swift
@@ -0,0 +1,18 @@
+//
+//  calendarGrid.swift
+//  Laugh Cry Think App
+//
+//  Created by Leticia Rios on 2/7/24.
+//
+
+import SwiftUI
+
+struct calendarGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    calendarGrid()
+}
