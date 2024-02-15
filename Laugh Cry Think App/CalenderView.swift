4e40e50c7faaf1415db527879a1d022349a0cee8
Quinn Davis <qdavis6106@stu.d214.org>
2024-02-15 10:57:03 -0600

    APIs Integrated + lots and lots of code cleanup

---
+++ Laugh Cry Think App/CalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderView.swift
+//  Laugh Cry Think App
+//
+//  Created by Quinn B. Davis on 2/15/24.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderView()
+}
